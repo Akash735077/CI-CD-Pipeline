da48fb6613398135485095d733f1c9dac347de4c
Akash Patil <135011385+Akash735077@users.noreply.github.com>
2025-05-22 09:35:42 +0530

    Create start_container.sh
    this is shell file to build ci/cd pipeline

---
+++ start_container.sh
@@ -0,0 +1,16 @@
+6.start_container.sh
+#!/bin/bash
+set -e
+
+# Pull the Docker image from Docker Hub
+echo
+
+# Run the Docker image as a container
+echo
+
+7.stop_container.sh
+#!/bin/bash
+set -e
+
+# Stop the running container (if any)
+echo "Hi"
